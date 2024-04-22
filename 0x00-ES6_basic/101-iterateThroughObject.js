7ae0c9e15e50a2bab2531e1bca7748fc00548994
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-04-22 23:51:25 +0100

    iterator function

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
   let result = '';
   for (const employee of reportWithIterator) {
-    result += employee.name + '|';
+    result += `${employee.name}|`;
   }
   // Remove the last '|' character
   result = result.slice(0, -1);
